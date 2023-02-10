35e79332571e5fbe318b95bae2bf3a8ebd36f670
Fereshtaha <90913150+Fereshtaha@users.noreply.github.com>
2023-02-10 11:25:20 +0100

    Added methods to come trackable objects classes

--- src/main/java/no/ntnu/EIVTR/TrackableObjects.java
+++ src/main/java/no/ntnu/EIVTR/TrackableObjects.java
@@ -1,11 +1,29 @@
 package no.ntnu.EIVTR;
 
-import java.util.Collections;
+import java.util.Collection;
 
 public class TrackableObjects {
-    public String nameOfObject;
-    Collections GazeData;
+    private String nameOfObject;
+    private Collection<Integer> GazeData;
 
+    public TrackableObjects(String nameOfObject, Collection<Integer> gazeData) {
+        this.nameOfObject = nameOfObject;
+        GazeData = gazeData;
+    }
 
+    public String getNameOfObject() {
+        return nameOfObject;
+    }
 
+    public void setNameOfObject(String nameOfObject) {
+        this.nameOfObject = nameOfObject;
+    }
+
+    public Collection<Integer> getGazeData() {
+        return GazeData;
+    }
+
+    public void setGazeData(Collection<Integer> gazeData) {
+        GazeData = gazeData;
+    }
 }
--- src/main/java/no/ntnu/EIVTR/TrackableObjectsService.java
+++ src/main/java/no/ntnu/EIVTR/TrackableObjectsService.java
@@ -1,4 +1,65 @@
 package no.ntnu.EIVTR;
 
+import javax.swing.text.html.Option;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
 public class TrackableObjectsService {
+    private TrackableObjectsRegister trackableObjectsRegister;
+
+    public TrackableObjectsService(TrackableObjectsRegister trackableObjectsRegister) {
+        this.trackableObjectsRegister = trackableObjectsRegister;
+    }
+
+    public List<TrackableObjects> iterableToList(Iterable<TrackableObjects> iterable) {
+        List<TrackableObjects> list = new LinkedList<>();
+        iterable.forEach(list::add);
+        return list;
+    }
+
+    public TrackableObjects findTrackableObjectByName(String name) {
+        Optional<TrackableObjects> trackableObjects = trackableObjectsRegister.findById(name);
+        return trackableObjects.orElse(null);
+    }
+
+    public List<TrackableObjects> getAll() {
+        return iterableToList(trackableObjectsRegister.findAll());
+    }
+
+    public boolean addNewTrackableObject(TrackableObjects trackableObjects) {
+        boolean added = false;
+        if (trackableObjects != null) {
+            trackableObjectsRegister.save(trackableObjects);
+            added = true;
+        }
+        return added;
+    }
+
+    public boolean deleteTrackableObject(TrackableObjects trackableObjects) {
+        boolean deleted = false;
+        if (trackableObjects != null) {
+            trackableObjectsRegister.delete(trackableObjects);
+            deleted = true;
+        }
+        return deleted;
+    }
+
+    public String updateTrackableObject(String name, TrackableObjects trackableObjects) {
+        TrackableObjects existingTrackableObject = findTrackableObjectByName(name);
+        String errorMessage = null;
+        if (existingTrackableObject == null) {
+            errorMessage = "No trackable object with " + name + "found";
+        }
+        if (trackableObjects == null) {
+            errorMessage = "Please check if your data is correct";
+        }
+        else if (trackableObjects.getNameOfObject() != name) {
+            errorMessage = "Please check your name, it does not match";
+        }
+        if (errorMessage == null) {
+            trackableObjectsRegister.save(trackableObjects);
+        }
+        return errorMessage;
+    }
 }
