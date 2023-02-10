8f18fa4893c333187136fc1210b2f2ec54899c0d
Fereshtaha <90913150+Fereshtaha@users.noreply.github.com>
2023-02-10 11:37:25 +0100

    Added documentation to trackableobject service class

--- src/main/java/no/ntnu/EIVTR/TrackableObjectsService.java
+++ src/main/java/no/ntnu/EIVTR/TrackableObjectsService.java
@@ -8,25 +8,48 @@ import java.util.Optional;
 public class TrackableObjectsService {
     private TrackableObjectsRegister trackableObjectsRegister;
 
+    /**
+     * Constructor with parameters
+     * @param trackableObjectsRegister TrackableObjectRegister
+     */
     public TrackableObjectsService(TrackableObjectsRegister trackableObjectsRegister) {
         this.trackableObjectsRegister = trackableObjectsRegister;
     }
 
+    /**
+     * Making an iterable to list
+     * @param iterable Iterable
+     * @return the list
+     */
     public List<TrackableObjects> iterableToList(Iterable<TrackableObjects> iterable) {
         List<TrackableObjects> list = new LinkedList<>();
         iterable.forEach(list::add);
         return list;
     }
 
+    /**
+     * Find a trackable object by name
+     * @param name String
+     * @return The trackable object or null if not found
+     */
     public TrackableObjects findTrackableObjectByName(String name) {
         Optional<TrackableObjects> trackableObjects = trackableObjectsRegister.findById(name);
         return trackableObjects.orElse(null);
     }
 
+    /**
+     * Get all trackable objects
+     * @return list of trackable objects
+     */
     public List<TrackableObjects> getAll() {
         return iterableToList(trackableObjectsRegister.findAll());
     }
 
+    /**
+     * Add new trackable objects
+     * @param trackableObjects Trackable objects
+     * @return true if added, false if not
+     */
     public boolean addNewTrackableObject(TrackableObjects trackableObjects) {
         boolean added = false;
         if (trackableObjects != null) {
@@ -36,6 +59,11 @@ public class TrackableObjectsService {
         return added;
     }
 
+    /**
+     * Delete a trackable object
+     * @param trackableObjects TrackableObject
+     * @return true if deleted, false if not
+     */
     public boolean deleteTrackableObject(TrackableObjects trackableObjects) {
         boolean deleted = false;
         if (trackableObjects != null) {
@@ -45,6 +73,13 @@ public class TrackableObjectsService {
         return deleted;
     }
 
+
+    /**
+     * Update trackable object by name
+     * @param name String
+     * @param trackableObjects TrackableObject
+     * @return error message telling whether it has been updated or not
+     */
     public String updateTrackableObject(String name, TrackableObjects trackableObjects) {
         TrackableObjects existingTrackableObject = findTrackableObjectByName(name);
         String errorMessage = null;
