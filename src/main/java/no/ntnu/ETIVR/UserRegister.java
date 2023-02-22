407ff0c74fa609cfa0162dc3774a85dab4ba8016
Malinbsy <Malinbsy@stud.ntnu.no>
2023-02-22 15:48:47 +0100

    Implemented UserRegister and UserService Class

--- src/main/java/no/ntnu/ETIVR/UserRegister.java
+++ src/main/java/no/ntnu/ETIVR/UserRegister.java
@@ -1,4 +1,44 @@
 package no.ntnu.ETIVR;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRegister {
+
+
+    /**
+     * @return
+     */
+    public List<User> getAllUsers();
+
+    /**
+     * @param user
+     * @return
+     */
+    public boolean addNewUser(User user);
+
+    /**
+     * @param id
+     * @return
+     */
+    public User findUserById(int id);
+
+    /**
+     * @param userId
+     * @return
+     */
+    public boolean deleteUser(int userId);
+
+
+    void delete(User value);
+
+    Optional<User> findById(int userId);
+
+    void save(User user);
+
+    Object findAll();
+
 }
+
+
+
--- src/main/java/no/ntnu/ETIVR/UserService.java
+++ src/main/java/no/ntnu/ETIVR/UserService.java
@@ -1,4 +1,83 @@
 package no.ntnu.ETIVR;
 
-public class UserService {
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+public class UserService implements UserRegister {
+
+    private UserRegister userRegister;
+
+
+    /**
+     *
+     * @param userRegister
+     */
+    public UserService(UserRegister userRegister){
+        this.userRegister = userRegister;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public List<User> getAllUsers(){
+        return (List<User>) userRegister.findAll();
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public User findUserById(int id){
+        Optional<User> user = userRegister.findById(id);
+        return user.get();
+    }
+
+
+
+    public boolean addNewUser(User user){
+        boolean added = false;
+        if(user != null && user.isValid()){
+            try{
+                findUserById(user.getUserId());
+            }catch (NoSuchElementException e){
+                user.setPassword();
+                userRegister.save(user);
+                added = true;
+            }
+        }
+        return added;
+    }
+
+
+    public boolean deleteUser(int userId) {
+        Optional<User> user = userRegister.findById((int) userId);
+        user.ifPresent(value -> userRegister.delete(value));
+        return user.isPresent();
+    }
+
+
+    @Override
+    public void delete(User value) {
+
+    }
+
+    @Override
+    public Optional<User> findById(int userId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public Object findAll() {
+        return null;
+    }
+
+
 }
