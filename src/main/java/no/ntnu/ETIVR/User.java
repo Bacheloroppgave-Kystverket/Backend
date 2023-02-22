4a1c248f44041da7f0033fb4ffc89f36961e4447
Malinbsy <Malinbsy@stud.ntnu.no>
2023-02-22 15:40:02 +0100

    Implemented User Class

--- src/main/java/no/ntnu/ETIVR/User.java
+++ src/main/java/no/ntnu/ETIVR/User.java
@@ -1,4 +1,90 @@
 package no.ntnu.ETIVR;
 
-public class User {
-}
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+/**
+     *
+     */
+    @Entity(name = "user")
+    public class User {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(unique = true)
+
+        private int userId;
+        private String userName;
+        private String password;
+        private boolean active = true;
+
+
+        /**
+         *
+         */
+        public User() {
+
+        }
+
+
+        @JsonCreator
+        public User(@JsonProperty("userId") int userId, @JsonProperty("userName") String userName, @JsonProperty("password") String password){
+            this.userId = userId;
+            this.userName = userName;
+            this.password = password;
+        }
+
+        /**
+         *
+         */
+        public boolean isActive() {
+            return active;
+        }
+
+        /**
+         *
+         */
+        public void setActive(){
+            this.active = active;
+        }
+
+        /**
+         *
+         * @return
+         */
+        public int getUserId(){
+            return userId;
+        }
+
+        /**
+         *
+         */
+        public void setUserId(){
+            this.userId =userId;
+        }
+
+        /**
+         *
+         * @return
+         */
+        public String getPassword() {
+            return password;
+        }
+
+        /**
+         *
+         */
+        public void setPassword(){
+            this.password = password;
+        }
+
+        /**
+         *
+         */
+        public boolean isValid(){
+            return !"".equals(userName);
+        }
+    }
+
