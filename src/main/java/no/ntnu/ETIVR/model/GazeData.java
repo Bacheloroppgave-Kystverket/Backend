3d629bccd9ba132b506a70c29769b073f9adec8e
Fereshta Ahmadi <fereshtaahmadi@dhcp-10-22-186-208.wlan.ntnu.no>
2023-02-23 11:42:50 +0100

    Added documentation to DazeData

--- src/main/java/no/ntnu/ETIVR/model/GazeData.java
+++ src/main/java/no/ntnu/ETIVR/model/GazeData.java
@@ -6,14 +6,24 @@ public class GazeData {
     private int fixations;
     private float fixationDuration;
 
+    /**
+     * Make an instance of GazeData
+     * @param locationID location ID
+     */
     public GazeData(String locationID) {
         this.locationID = locationID;
     }
 
+    /**
+     * Increment fixation
+     */
     public void incrementFixation() {
         fixations++;
     }
 
+    /**
+     * Adding delta time to fixation duration
+     */
     public void addTime() {
         long time = System.currentTimeMillis();
         long currentTime = System.currentTimeMillis();
@@ -21,14 +31,26 @@ public class GazeData {
         fixationDuration += changeInTime;
     }
 
+    /**
+     * Get location ID
+     * @return location ID
+     */
     public String getLocationID() {
         return locationID;
     }
 
+    /**
+     * Get fixations
+     * @return fixation
+     */
     public int getFixations() {
         return fixations;
     }
 
+    /**
+     * Get fixation duration
+     * @return fixation duration
+     */
     public float getFixationDuration() {
         return fixationDuration;
     }
