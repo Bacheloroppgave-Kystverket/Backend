28eeed742a3fa64407149cd43306b28db7df63a2
Malinbsy <Malinbsy@stud.ntnu.no>
2023-03-02 11:47:21 +0100

    Added UsertTest class and tests

--- src/main/java/no/ntnu/ETIVR/model/User.java
+++ src/main/java/no/ntnu/ETIVR/model/User.java
@@ -5,9 +5,12 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
 
 
-    @Entity(name = "user")
+@Entity(name = "user")
     public class User {
 
         @Id
@@ -37,8 +40,11 @@ import javax.persistence.*;
      */
         @JsonCreator
         public User(@JsonProperty("userId") long userId, @JsonProperty("userName") String userName, @JsonProperty("password") String password){
+            checkIfNumberNotNegative(userId,"UserId");
             this.userId = userId;
+            checkIfObjectIsNull(userName, "UserName");
             this.userName = userName;
+            checkIfObjectIsNull(password,"Password");
             this.password = password;
         }
 
@@ -87,5 +93,52 @@ import javax.persistence.*;
     public void setPassword(){
             this.password = password;
         }
+
+    /**
+     * Checks if an input date is before or equal to today's date.
+     * @param localDateTime the date to check.
+     */
+    private void checkIfDateIsBeforeOrEqualToCurrentDate(LocalDateTime localDateTime){
+        checkIfObjectIsNull(localDateTime, "date");
+        if (LocalDate.now().isBefore(ChronoLocalDate.from(localDateTime))){
+            throw new IllegalArgumentException("The set date is after current date.");
+        }
     }
+
+    /**
+     * Checks if an object is null.
+     *
+     * @param object the object you want to check.
+     * @param error  the error message the exception should have.
+     */
+    private void checkIfObjectIsNull(Object object, String error) {
+        if (object == null) {
+            throw new IllegalArgumentException("The " + error + " cannot be null.");
+        }
+    }
+
+    /**
+     * Checks if an int number is not negative
+     * @param object int object to be checked
+     * @param error String
+     */
+    private void checkIfIntNumberNotNegative(int object, String error) {
+        if (object <= 0) {
+            throw new IllegalArgumentException("The " + error + " Cannot be negative values.");
+        }
+    }
+
+
+    /**
+     * Check to make sure that integer values cannot be negative.
+     * @param object the object to be checked.
+     * @param error exception message to be displayed.
+     */
+    private void checkIfNumberNotNegative(long object, String error) {
+        if (object <= 0) {
+            throw new IllegalArgumentException("The " + error + " Cannot be negative values.");
+        }
+    }
+
+}
 
---
+++ src/test/java/no/ntnu/ETIVR/model/UserTest.java
@@ -0,0 +1,55 @@
+package no.ntnu.ETIVR.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserTest extends DefaultTest{
+
+    @Override
+    @BeforeEach
+    public void SetupTestData() {
+        setUpStringBuilder();
+    }
+
+    @Override
+    @AfterEach
+    public void checkForErrors() {
+        checkIfTestsFailedAndDisplayResult();
+
+    }
+
+    @Test
+    @DisplayName("Tests if the constructor works with valid input")
+    void testConstructorValidInput(){
+        try {
+            User user = new User(2l, "John","1t3b");
+        }catch (IllegalArgumentException e){
+            addErrorWithException("Expected the user", "to be made", e);
+        }
+    }
+
+    @Test
+    @DisplayName("Test if the constructor works with invalid input")
+    void testConstructorInvalidInput(){
+        User user;
+        try {
+            user = new User(-2l,"John","1t3b");
+            addError(getIllegalPrefix(),"the user id can not be under 0");
+        }catch (IllegalArgumentException e){
+        }
+        try {
+            user = new User(2l, null,"1t3b");
+            addError(getIllegalPrefix(),"the username can not be null");
+        }catch (IllegalArgumentException e){
+
+        }
+        try {
+            user = new User(2l,"John",null);
+            addError(getIllegalPrefix(),"password can not be null");
+        } catch (IllegalArgumentException E){
+
+        }
+    }
+}
